7aec275a6b448adf373eb2b6f145c56986f3bfa4
atsuky <andreugallofre@gmail.com>
2016-04-25 01:11:28 +0200

    Create blade.ino

---
+++ blade.ino
@@ -0,0 +1,52 @@
+#include <Adafruit_NeoPixel.h>         // Include Adafruit NeoPixel library
+#define PIN            8                // LED on digital pin 6
+#define NUMLEDS        16                 // Use total of 1 LED
+ 
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUMLEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+int blade[16] = {1,0,0,1,0,1,1,0,0,1,1,0,1,0,0,1}; //Vector with blade shape
+
+void setup() {
+
+  leds.begin();     // Initialize 'leds' object
+  off();            // Turn off all leds
+  leds.show();      // Comit changes to leds
+  delay(500);       
+}
+
+void loop() {
+
+run_blade();
+run_out();
+
+}
+void off(){ //Function to turn off all leds
+  for (int i=0; i<NUMLEDS;i++)leds.setPixelColor(i, leds.Color(0, 0, 0));
+  leds.show(); 
+}
+
+void run_blade(){ //Function to turn of "blade" leds
+for(int i=0; i<NUMLEDS ;i++){
+  if (blade[i] == 1) leds.setPixelColor(i, leds.Color(0, 10, 0));
+  }
+  leds.show();
+  for(int i=0; i<NUMLEDS ;i++){
+  if (blade[i] == 1) leds.setPixelColor(i, leds.Color(0, 0, 0));
+  }
+  delay(300);
+}
+
+void run_ball(){ //Function to turn on "ball" leds
+for(int i=0; i<NUMLEDS ;i++){
+  if (blade[i] == 0) leds.setPixelColor(i, leds.Color(0, 10, 0));
+  }
+  leds.show();
+  for(int i=0; i<NUMLEDS ;i++){
+  if (blade[i] == 0) leds.setPixelColor(i, leds.Color(0, 0, 0));
+  }
+  delay(300);
+}
+
+
+
+
