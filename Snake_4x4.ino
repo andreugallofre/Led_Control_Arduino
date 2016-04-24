3104577575a4a3d5336aed01981a7c3d9a84db53
atsuky <andreugallofre@gmail.com>
2016-04-25 01:44:40 +0200

    Update Snake_4x4.ino

--- Snake_4x4.ino
+++ Snake_4x4.ino
@@ -5,8 +5,10 @@
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUMLEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 int Snake4_4[16] = {0,1,2,3,7,6,5,4,8,9,10,11,15,14,13,12};
+int i = 0;
 
 void setup() {
+
   leds.begin(); // Initialize 'leds' object
   off();        // Turn off all leds
   leds.show();  // Comit changes to leds
@@ -15,19 +17,23 @@ void setup() {
 
 void loop() {
 
-//Loop to run an snake acording to the vector declared first for a 4x4 matrix  
-for(int i=0; i<NUMLEDS ;i++){
+  int previous = i-1;
+  int previous2 = i-2;
+
+  if (previous < 0) previous+=16;
+  if (previous2 < 0) previous2+=16;
+  
   leds.setPixelColor(Snake4_4[i], leds.Color(255, 0, 0));
-  leds.setPixelColor(Snake4_4[i+1], leds.Color(255, 0, 0));
-  leds.setPixelColor(Snake4_4[i+2], leds.Color(255, 0, 0));
+  leds.setPixelColor(Snake4_4[previous], leds.Color(255, 0, 0));
+  leds.setPixelColor(Snake4_4[previous2], leds.Color(255, 0, 0));
   leds.show();
   leds.setPixelColor(Snake4_4[i], leds.Color(0, 0, 0));
-  leds.setPixelColor(Snake4_4[i+1], leds.Color(0, 0, 0));
-  leds.setPixelColor(Snake4_4[i+2], leds.Color(0, 0, 0));
-  delay(150);
-}
-}
-
+  leds.setPixelColor(Snake4_4[previous], leds.Color(0, 0, 0));
+  leds.setPixelColor(Snake4_4[previous2], leds.Color(0, 0, 0));
+  delay(500);
+  i = (i+1)%16;
+  }
+  
 //Function to turn down all leds
 void off(){
   for (int i=0; i<NUMLEDS;i++)leds.setPixelColor(i, leds.Color(0, 0, 0));
